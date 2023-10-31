153c44de4a826b638ac9e6f11a0c35d78676f661
Alex Lepin <alexlepin@mail.ru>
2023-10-31 22:36:35 +0300

    The part-1 of the hw-2 is in process

---
+++ hw-2/part-1/script.js
@@ -0,0 +1,50 @@
+"use strict";
+new Vue({
+  el: ".news__cards",
+  data: {
+    articles: [
+      {
+        imgUrl: "img/news__img-1.png",
+        imgAlt: "kitchen",
+        imgTitle: "Kitchan Design",
+        cardHeading: "Let’s Get Solution For Building Construction Work",
+        cardDate: "26 December,2022",
+      },
+      {
+        imgUrl: "img/news__img-2.png",
+        imgAlt: "kitchen",
+        imgTitle: "Living Design",
+        cardHeading: "Low Cost Latest Invented Interior Designing Ideas",
+        cardDate: "22 December,2022",
+      },
+      {
+        imgUrl: "img/news__img-3.png",
+        imgAlt: "kitchen",
+        imgTitle: "Interior Design",
+        cardHeading: "Best For Any Office & Business Interior Solution",
+        cardDate: "25 December,2022",
+      },
+      {
+        imgUrl: "img/news__img-4.png",
+        imgAlt: "kitchen",
+        imgTitle: "Kitchan Design",
+        cardHeading: "Let’s Get Solution For Building Construction Work",
+        cardDate: "26 December,2022",
+      },
+      {
+        imgUrl: "img/news__img-5.png",
+        imgAlt: "kitchen",
+        imgTitle: "Living Design",
+        cardHeading: "Low Cost Latest Invented Interior Designing Ideas",
+        cardDate: "22 December,2022",
+      },
+      {
+        imgUrl: "img/news__img-6.png",
+        imgAlt: "kitchen",
+        imgTitle: "Interior Design",
+        cardHeading: "Best For Any Office & Business Interior Solution",
+        cardDate: "25 December,2022",
+      },
+    ],
+  },
+});
