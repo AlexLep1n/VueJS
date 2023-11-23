224633958584397672583303044ce0e81f133bed
Alex Lepin <alexlepin@mail.ru>
2023-11-23 21:16:48 +0300

    The part-2 of the hw-4 is in progress

---
+++ hw-4/src/components/ProductDetailsComp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>{{ productItem.name }}</h3>
+    <p>{{ productItem.price }}</p>
+    <p>{{ productItem.available }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetailsComp',
+  props: ['productItem'],
+
+  data () {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ hw-4/src/components/ProductItemComp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ProductDetailsComp></ProductDetailsComp>
+  </div>
+</template>
+
+<script>
+import ProductDetailsComp from "./ProductDetailsComp.vue";
+
+export default {
+  name: "ProductItemComp",
+  data() {
+    return {};
+  },
+  methods: {},
+  components: { ProductDetailsComp },
+};
+</script>
+
+<style lang="scss" scoped></style>
