21aafc234af6dbfe5688c33b22df6aefac448078
Alex Lepin <alexlepin@mail.ru>
2023-12-02 20:58:39 +0300

    project details page has been completed

--- project/src/components/parts/SwiperPart.vue
+++ project/src/components/parts/SwiperPart.vue
@@ -1,25 +1,37 @@
 <template>
-  <Swiper :pagination="true">
-    <SwiperSlide v-for="(item, index) in swiperProjectDetails" :key="index">
+  <!-- <Swiper :pagination="true">
+    <swiper-slide v-for="(item, index) in swiperProjectDetails" :key="index">
       <img :src="item.imgUrl" :alt="item.Alt" />
-    </SwiperSlide>
-  </Swiper>
+    </swiper-slide>
+  </Swiper> -->
+  <div>
+    <img
+      class="swipper__img"
+      :src="swiperProjectDetails[0].imgUrl"
+      :alt="swiperProjectDetails[0].imgAlt"
+    />
+    <div class="pagination">
+      <i class="fa-regular fa-circle"></i>
+      <i class="fa-regular fa-circle"></i>
+      <i class="fa-regular fa-circle"></i>
+    </div>
+  </div>
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from "swiper/vue";
-import { Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/pagination";
+// import { Swiper, SwiperSlide } from "swiper/vue";
+// import { Pagination } from "swiper/modules";
+// import "swiper/css";
+// import "swiper/css/pagination";
 import { mapGetters } from "vuex";
 
 export default {
   name: "ProjectSwiperPart",
-  setup() {
-    return {
-      modules: [Pagination],
-    };
-  },
+  // setup() {
+  //   return {
+  //     modules: [Pagination],
+  //   };
+  // },
 
   methods: {},
 
@@ -27,30 +39,25 @@ export default {
     ...mapGetters(["swiperProjectDetails"]),
   },
   components: {
-    Swiper,
-    SwiperSlide,
+    // Swiper,
+    // SwiperSlide,
   },
 };
 </script>
 
 <style lang="scss" scoped>
-// .swiper {
-//   width: 100%;
-//   height: 100%;
-// }
+.swipper {
+  &__img {
+    border: none;
+    border-radius: 70px;
+    margin-bottom: 10px;
+  }
+}
 
-// .swiper-slide {
-//   text-align: center;
-//   font-size: 18px;
-//   background: #fff;
-
-//   /* Center slide text vertically */
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// }
-.swiper-slide img {
-  border: none;
-  border-radius: 70px;
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
 }
 </style>
