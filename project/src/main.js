5012772dabc99e2e32757846381102ed82936e05
Alex Lepin <alexlepin@mail.ru>
2023-12-02 14:04:05 +0300

    change: recreated vue project

--- project/src/main.js
+++ project/src/main.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App.vue'
-import store from './store/index.js'
-
-Vue.use(Vuex)
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: (h) => h(App),
-  store: new Vuex.Store(store)
+  store,
+  render: h => h(App)
 }).$mount('#app')
--- project/src/store/index.js
+++ project/src/store/index.js
@@ -1,5 +1,17 @@
-export default {
-  state: {},
-  getters: {},
-  mutations: {},
-};
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
